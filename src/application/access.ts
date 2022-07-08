1471f429a5e6ed2275a0004aac34899791c678b8
muhad-bk <muhad.bk@naicoits.com>
2022-07-09 00:10:45 +0530

    permisssions system

--- src/application/access.ts
+++ src/application/access.ts
@@ -1,5 +1,6 @@
 import { TimestampFieldConfig } from "@keystone-6/core/fields";
 import { BaseListTypeInfo } from "@keystone-6/core/types";
+import { ListAccessArgs } from "./types";
 
 export const fieldOptions: TimestampFieldConfig<BaseListTypeInfo> = {
   access: {
@@ -16,4 +17,88 @@ export const fieldOptions: TimestampFieldConfig<BaseListTypeInfo> = {
     },
   },
   graphql: { omit: ["update", "create"] },
+};
+
+/*
+  The basic level of access to the system is being signed in as a valid user. This gives you access
+  to the Admin UI, access to your own User and Todo items, and read access to roles.
+*/
+export const isSignedIn = ({ session }: ListAccessArgs) => {
+  return !!session;
+};
+
+/*
+  Permissions are shorthand functions for checking that the current user's role has the specified
+  permission boolean set to true
+*/
+export const permissions = {
+  canCreateTodos: ({ session }: ListAccessArgs) =>
+    !!session?.data.role?.canCreateTodos,
+  canManageAllTodos: ({ session }: ListAccessArgs) =>
+    !!session?.data.role?.canManageAllTodos,
+  canManagePeople: ({ session }: ListAccessArgs) =>
+    !!session?.data.role?.canManagePeople,
+  canManageRoles: ({ session }: ListAccessArgs) =>
+    !!session?.data.role?.canManageRoles,
+};
+
+/*
+  Rules are logical functions that can be used for list access, and may return a boolean (meaning
+  all or no items are available) or a set of filters that limit the available items
+*/
+export const rules = {
+  canReadTodos: ({ session }: ListAccessArgs) => {
+    if (!session) {
+      // No session? No todos.
+      return false;
+    } else if (session.data.role?.canManageAllTodos) {
+      // Can see all todos that are: assigned to them, or not private
+      return {
+        OR: [
+          { assignedTo: { id: { equals: session.itemId } } },
+          { assignedTo: null, isPrivate: { equals: true } },
+          { NOT: { isPrivate: { equals: true } } },
+        ],
+      };
+    } else {
+      // Can only see their own todos
+      return { assignedTo: { id: { equals: session.itemId } } };
+    }
+  },
+  canManageTodos: ({ session }: ListAccessArgs) => {
+    if (!session) {
+      // No session? No todos.
+      return false;
+    } else if (session.data.role?.canManageAllTodos) {
+      // Can manage todos? go for it
+      return true;
+    } else {
+      // Can only manage their own todos
+      return { assignedTo: { id: { equals: session.itemId } } };
+    }
+  },
+  canReadPeople: ({ session }: ListAccessArgs) => {
+    if (!session) {
+      // No session? No people.
+      return false;
+    } else if (session.data.role?.canSeeOtherPeople) {
+      // Can see everyone
+      return true;
+    } else {
+      // Can only see yourself
+      return { id: { equals: session.itemId } };
+    }
+  },
+  canUpdatePeople: ({ session }: ListAccessArgs) => {
+    if (!session) {
+      // No session? No people.
+      return false;
+    } else if (session.data.role?.canEditOtherPeople) {
+      // Can update everyone
+      return true;
+    } else {
+      // Can update yourself
+      return { id: { equals: session.itemId } };
+    }
+  },
 };
---
+++ src/entities/roles.ts
@@ -0,0 +1,75 @@
+import {
+  checkbox,
+  image,
+  password,
+  relationship,
+  select,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
+import { list } from "@keystone-6/core";
+import { isSignedIn, permissions, rules } from "../application/access";
+
+export const Role = list({
+  /*
+      SPEC
+      - [x] Block all public access
+      - [x] Restrict edit access based on canManageRoles
+      - [ ] Prevent users from deleting their own role
+      - [ ] Add a pre-save hook that ensures some permissions are selected when others are:
+          - [ ] when canEditOtherPeople is true, canSeeOtherPeople must be true
+          - [ ] when canManagePeople is true, canEditOtherPeople and canSeeOtherPeople must be true
+      - [ ] Extend the Admin UI with client-side validation based on the same set of rules
+    */
+  access: {
+    operation: {
+      create: permissions.canManageRoles,
+      query: isSignedIn,
+      update: permissions.canManageRoles,
+      delete: permissions.canManageRoles,
+    },
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    listView: {
+      initialColumns: ["name", "assignedTo"],
+    },
+    itemView: {
+      defaultFieldMode: (args) =>
+        permissions.canManageRoles(args) ? "edit" : "read",
+    },
+  },
+  fields: {
+    /* The name of the role */
+    name: text({ validation: { isRequired: true } }),
+    /* Create Todos means:
+         - create todos (can only assign them to others with canManageAllTodos) */
+    canCreateTodos: checkbox({ defaultValue: false }),
+    /* Manage All Todos means:
+         - create new Todo items and assign them to someone else (with canCreateTodos)
+         - update and delete Todo items not assigned to the current user */
+    canManageAllTodos: checkbox({ defaultValue: false }),
+    /* See Other Users means:
+         - list all users in the database (users can always see themselves) */
+    canSeeOtherPeople: checkbox({ defaultValue: false }),
+    /* Edit Other Users means:
+         - edit other users in the database (users can always edit their own item) */
+    canEditOtherPeople: checkbox({ defaultValue: false }),
+    /* Manage Users means:
+         - change passwords (users can always change their own password)
+         - assign roles to themselves and other users */
+    canManagePeople: checkbox({ defaultValue: false }),
+    /* Manage Roles means:
+         - create, edit, and delete roles */
+    canManageRoles: checkbox({ defaultValue: false }),
+    /* This list of People assigned to this role */
+    assignedTo: relationship({
+      ref: "Person.role",
+      many: true,
+      ui: {
+        itemView: { fieldMode: "read" },
+      },
+    }),
+  },
+});
