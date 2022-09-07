a5f9b33d08bc82bfd91ad9a71cb3cc6d05bc22cc
Muhad BK <mr.muhad@gmail.com>
2022-09-07 23:50:31 +0530

    header name in pascal

--- src/entities/excel-upload.ts
+++ src/entities/excel-upload.ts
@@ -5,49 +5,70 @@ import excelFileToJson from "../services/excelFileToJson";
 import { Vehicle } from "@prisma/client";
 
 interface VehicleDTO {
-  Loan_Agreement_No: string;
-  Customer_Name: string;
-  Make_Model: string;
-  Variant: string;
-  Categoty: string;
-  "Fuel Type": string;
-  RC_Status: string;
-  Registration_Number: string;
-  Year_of_Manufacture: string;
-  Ownership: number;
-  Mileage: string;
+  loan_agreement_no: string;
+  customer_name: string;
+  make_model: string;
+  variant: string;
+  categoty: string;
+  fuel_type: string;
+  power_steering: string;
+  autobse_contact:string;
+  autobse_contact_person:string;
+  vehicle_condition:string;
+  shape:string;
+  color:string;
+  state:string;
+  city: string;
+  area:string;
+  yard_name:string;
+  payment_terms:string;
+  date_of_registration:string;
+  tax_type:string;
+  tax_validity_date:string;
+  rc_status: string;
+  registration_number: string;
+  year_of_manufacture: string;
+  ownership: number;
+  mileage: string;
   km : number;
-  Quote_increament: number;
-  Insurance_Status: string;
-  Yard_Location: string;
-  Start_Price: number;
-  Reserve_Price: number;
-  Repo_Dt: string;
-  Veicle_Location: string;
-  Vehicle_Remarks: string;
-  "Auction Manager": string;
-  "Action Manager": string;
-  "Seller Name": string;
-  "Parking Charges": number;
-  Insurance: string;
-  "Insurance Valid Till": string;
-  Tax: string;
-  Fitness: string;
-  Permit: string;
-  "Engine No": string;
-  "Chassis NO": string;
+  quote_increament: number;
+  insurance_status: string;
+  yard_location: string;
+  hypothication: string;
+  climate_control: string;
+  door_count:string;
+  gear_box:string;
+  buyer_fees: string;
+  rto_fine:string;
+  parking_rate: string;
+  approx_parking_charges: string;
+  client_contact_person: string;
+  client_contact_no: string;
+  additional_remarks:string;
+  start_price: number;
+  reserve_price: number;
+  repo_dt: string;
+  veicle_location: string;
+  vehicle_remarks: string;
+  auction_manager: string;
+  seller_name: string;
+  parking_charges: number;
+  insurance: string;
+  insurance_type:string;
+  insurance_valid_till: string;
+  insurance_expiry_date: string;
+  tax: string;
+  fitness: string;
+  permit: string;
+  engine_no: string;
+  chassis_no: string;
   front_image: string;
   back_image: string;
   left_image: string;
   right_image: string;
-  "INSPECTION LINK": string;
-  "image 6": string;
-  "image 7": string;
-  "image 8": string;
-  "image 9": string;
-  "image 10": string;
-  "image 11": string;
-  "image 12": string;
+  inspection_link: string;
+  image_6: string;
+  image_5: string;
 }
 
 export const ExcelUpload = list({
@@ -79,52 +100,78 @@ export const ExcelUpload = list({
               query: `endDate startDate eventCategory vehicleLiveTimeIn gapInBetweenVehicles`,
             }),
           ]);
-          // console.log("data", data);
           if (data.length > 0) {
             const vehicles: Vehicle[] = data.map(
-              (vehicleItem: VehicleDTO, i: number) => {
+              (vehicle:any, i: number) => {
+                const vehicleItem = {} as VehicleDTO;
+                Object.keys(vehicle).forEach(key => 
+                  // @ts-ignore
+                  {vehicleItem[`${key?.trim()?.toLowerCase()?.replaceAll(' ', '_').replaceAll('.', '')}`] = vehicle[key]
+                });
+                // console.log("vehicleItem: ", vehicleItem);
                 return {
                   registrationNumber:
-                    vehicleItem.Registration_Number.toUpperCase().trim(),
+                    vehicleItem.registration_number?.toUpperCase()?.trim(),
                   loanAgreementNo:
-                    vehicleItem.Loan_Agreement_No?.toUpperCase()?.trim(),
-                  registeredOwnerName:vehicleItem.Customer_Name?.trim(),
-                  make: vehicleItem.Make_Model?.toLowerCase()?.trim(),
-                  model: vehicleItem.Make_Model?.toLowerCase()?.trim(),
-                  varient: vehicleItem.Variant?.toLowerCase()?.trim(),
-                  categoty: vehicleItem.Categoty?.toLowerCase()?.trim(),
-                  fuel: vehicleItem["Fuel Type"]?.toLowerCase()?.trim(),
-                  rcStatus: vehicleItem.RC_Status?.toLowerCase()?.trim(),
-                  yearOfManufacture: Number(vehicleItem.Year_of_Manufacture)||0,
-                  ownership: Number(vehicleItem.Ownership)||0,
-                  mileage: Number(vehicleItem.Mileage)||0,
+                    vehicleItem.loan_agreement_no?.toUpperCase()?.trim(),
+                  registeredOwnerName:vehicleItem.customer_name?.trim(),
+                  make: vehicleItem.make_model?.toLowerCase()?.trim(),
+                  model: vehicleItem.make_model?.toLowerCase()?.trim(),
+                  varient: vehicleItem.variant?.toLowerCase()?.trim(),
+                  categoty: vehicleItem.categoty?.toLowerCase()?.trim(),
+                  fuel: vehicleItem.fuel_type?.toLowerCase()?.trim(),
+                  rcStatus: vehicleItem.rc_status?.toLowerCase()?.trim(),
+                  yearOfManufacture: Number(vehicleItem.year_of_manufacture)||0,
+                  ownership: Number(vehicleItem.ownership)||0,
+                  mileage: Number(vehicleItem.mileage)||0,
                   kmReading: Number(vehicleItem.km)||0,
-                  quoteIncreament: Number(vehicleItem.Quote_increament)||1000,
-                  insuranceStatus: vehicleItem.Insurance_Status?.toUpperCase()?.trim(),
-                  yardLocation: vehicleItem.Yard_Location?.toUpperCase()?.trim(),
-                  startPrice: Number(vehicleItem.Start_Price)||0,
-                  reservePrice: Number(vehicleItem.Reserve_Price)||0,
-                  repoDt: new Date(Date.parse(vehicleItem.Repo_Dt)||0),
-                  veicleLocation: vehicleItem.Veicle_Location?.toUpperCase()?.trim(),
-                  vehicleRemarks: vehicleItem.Vehicle_Remarks,
-                  auctionManager: vehicleItem["Auction Manager"]?.toString(),
-                  actionManager: vehicleItem["Action Manager"],
-                  sellerName: vehicleItem["Seller Name"],
-                  parkingCharges: vehicleItem["Parking Charges"]?.toString(),
-                  insurance: vehicleItem.Insurance,
-                  insuranceValidTill: vehicleItem["Insurance Valid Till"],
-                  tax: vehicleItem.Tax,
-                  fitness: vehicleItem.Fitness,
-                  permit: vehicleItem.Permit,
-                  engineNo: vehicleItem["Engine No"],
-                  chassisNo: vehicleItem["Chassis NO"],
+                  quoteIncreament: Number(vehicleItem.quote_increament)||1000,
+                  insuranceStatus: vehicleItem.insurance_status?.toUpperCase()?.trim(),
+                  yardLocation: vehicleItem.yard_name?.trim()?? vehicleItem.yard_location?.trim(),
+                  startPrice: Number(vehicleItem.start_price)||0,
+                  reservePrice: Number(vehicleItem.reserve_price)||0,
+                  repoDt: new Date(vehicleItem.repo_dt),
+                  veicleLocation: vehicleItem.veicle_location?.toUpperCase()?.trim(),
+                  vehicleRemarks: vehicleItem.additional_remarks ?? vehicleItem.vehicle_remarks,
+                  auctionManager: vehicleItem.auction_manager?.toString(),
+                  parkingCharges: vehicleItem.parking_charges?.toString(),
+                  insurance: vehicleItem.insurance_type??vehicleItem.insurance,
+                  insuranceValidTill: new Date(vehicleItem.insurance_expiry_date)?? new Date (vehicleItem.insurance_valid_till),
+                  tax: vehicleItem.tax_type??vehicleItem.tax,
+                  taxValidityDate: new Date(vehicleItem.tax_validity_date),
+                  fitness: vehicleItem.fitness,
+                  permit: vehicleItem.permit,
+                  engineNo: vehicleItem.engine_no,
+                  chassisNo: vehicleItem.chassis_no,
                   frontImage: vehicleItem.front_image?.trim(),
                   backImage: vehicleItem.back_image?.trim(),
                   leftImage: vehicleItem.left_image?.trim(),
                   rightImage: vehicleItem.right_image?.trim(),
-                  image5: vehicleItem["image 5"].trim(),
-                  image6: vehicleItem["image 6"].trim(),
-                  inspectionLink: vehicleItem["INSPECTION LINK"]?.trim(),
+                  image5: vehicleItem.image_5?.trim(),
+                  image6: vehicleItem.image_6?.trim(),
+                  inspectionLink: vehicleItem.inspection_link?.trim(),
+                  autobseContact: vehicleItem.autobse_contact.toString(),
+                  autobse_contact_person: vehicleItem.autobse_contact_person?.trim(),
+                  vehicleCondition: vehicleItem.vehicle_condition?.trim(),
+                  powerSteering:vehicleItem.power_steering?.toUpperCase()?.trim(),
+                  shape:vehicleItem.shape?.toUpperCase()?.trim(),
+                  color: vehicleItem.color?.toUpperCase()?.trim(),
+                  state: vehicleItem.state?.toUpperCase()?.trim(),
+                  city: vehicleItem.city?.toUpperCase()?.trim(),
+                  area: vehicleItem.area?.toUpperCase()?.trim(),
+                  paymentTerms:vehicleItem.payment_terms,
+                  dateOfRegistration: new Date(vehicleItem.date_of_registration),
+                  hypothication: vehicleItem.hypothication,
+                  climateControl: vehicleItem.climate_control,
+                  doorCount: Number(vehicleItem.door_count) || 0,
+                  gearBox: vehicleItem.gear_box,
+                  buyerFees: vehicleItem.buyer_fees,
+                  rtoFine:vehicleItem.rto_fine,
+                  parkingRate: vehicleItem.parking_rate,
+                  approxParkingCharges: vehicleItem.approx_parking_charges,
+                  clientContactPerson: vehicleItem.client_contact_person,
+                  clientContactNo: vehicleItem.client_contact_no.toString(),
+                  additionalRemarks: vehicleItem.additional_remarks,
                   ExcelFile: { connect: { id: item.id } },
                   event: resolvedData.event,
                   bidTimeExpire:
@@ -150,11 +197,12 @@ export const ExcelUpload = list({
                         )
                       : new Date(eventData.startDate),
                   currentBidAmount: 0,
-                  startBidAmount: Number(vehicleItem.Start_Price) || 0,
+                  startBidAmount: Number(vehicleItem.start_price) || 0,
                   bidStatus: "pending",
                 };
               }
             );
+            // console.log("vehicles: ", vehicles)
             await context.query.Vehicle.createMany({
               data: vehicles,
             });
